0f746ab3f7aea7d9c3138b151ca5b721dfe7d342
Tanson Lee <leetanson@gmail.com>
2024-02-10 19:30:11 -0500

    both appraches

--- 1463_cherry_pickup_ii/main.py
+++ 1463_cherry_pickup_ii/main.py
@@ -7,6 +7,36 @@ class Solution:
 
         directions = [-1, 0, 1]
 
+        #####
+        # Bottom up approach
+        #####
+        # dp[row][p1][p2] = best you can do up to row where robots end at p1 and p2
+        dp = [[[float("-inf") for _ in range(cols)] for _ in range(cols)] for _ in range(rows)]
+
+        dp[0][0][cols - 1] = grid[0][0] + grid[0][cols - 1]
+        
+        for row in range(1, rows):
+            for c1 in range(cols):
+                for c2 in range(cols):
+                    # Each of c1 and c2 could have been arrived at by up to 3 places
+                    for dc1 in directions:
+                        for dc2 in directions:
+                            from_c1 = c1 + dc1
+                            from_c2 = c2 + dc2
+                            if from_c1 < 0 or from_c1 >= cols or from_c2 < 0 or from_c2 >= cols:
+                                continue
+                            dp[row][c1][c2] = max(dp[row][c1][c2], dp[row - 1][from_c1][from_c2])
+                    if c1 != c2:
+                        dp[row][c1][c2] += grid[row][c1] + grid[row][c2]
+                    else:
+                        dp[row][c1][c2] += grid[row][c1]
+
+        return max([max(x) for x in dp[-1]])
+
+
+        #####
+        # Top down approach
+        #####
         @cache
         def helper(curr_row, pos1, pos2):
             if curr_row == rows or pos1 < 0 or pos1 >= cols or pos2 < 0 or pos2 >= cols:

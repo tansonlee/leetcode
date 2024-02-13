6b4264701806574fff1f79ed12df184983b681eb
Tanson Lee <leetanson@gmail.com>
2024-02-12 20:09:23 -0500

    2108

---
+++ 2108_find_first_palindromic_string_in_the_array/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for s in words:
+            if s == s[::-1]:
+                return s
+        return ""
+        

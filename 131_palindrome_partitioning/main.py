cbfcfdd02eeeae17e0db9b94c9414618b7547e27
Tanson Lee <leetanson@gmail.com>
2024-05-21 23:00:31 -0700

    131 cleaner

--- 131_palindrome_partitioning/main.py
+++ 131_palindrome_partitioning/main.py
@@ -1,22 +1,13 @@
-from functools import cache
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        @cache
-        def is_palindrome(s):
-            return s == s[::-1]
+        if len(s) == 0:
+            return [[]]
+        result = []
+        for i in range(1, len(s) + 1):
+            prefix = s[:i]
+            if prefix == prefix[::-1]:
+                suffix_partitions = self.partition(s[i:])
+                for sp in suffix_partitions:
+                    result.append([prefix] + sp)
+        return result
         
-        @cache
-        def helper(s):
-            if len(s) == 0:
-                return [[]]
-            result = []
-            for i in range(len(s)):
-                if is_palindrome(s[:i + 1]):
-                    rest = helper(s[i + 1:])
-                    for ans in rest:
-                        result.append([s[:i + 1]] + ans)
-            return result
-        
-        return helper(s)
-

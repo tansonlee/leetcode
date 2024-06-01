18c295dd7f73de8151f3f082ca46cabcab765f6a
Tanson Lee <leetanson@gmail.com>
2024-06-01 10:46:47 -0700

    129 iterative

--- 129_sum_root_to_leaf_numbers/main.py
+++ 129_sum_root_to_leaf_numbers/main.py
@@ -18,3 +18,32 @@ class Solution:
         
         return helper(root, 0)
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        result = 0
+        stack = [(root, root.val)]
+
+        while stack:
+            node, val = stack.pop()
+
+            if node.left is None and node.right is None:
+                result += val
+                continue
+            
+            if node.left:
+                stack.append((node.left, val * 10 + node.left.val))
+            if node.right:
+                stack.append((node.right, val * 10 + node.right.val))
+        
+        return result
+
+

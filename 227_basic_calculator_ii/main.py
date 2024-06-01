0181e7cafab41e7fb5a5822c898991050dafefa3
Tanson Lee <leetanson@gmail.com>
2024-06-01 12:54:56 -0700

    227 alt

--- 227_basic_calculator_ii/main.py
+++ 227_basic_calculator_ii/main.py
@@ -39,3 +39,43 @@ class Solution:
         return sum(stack)
 
 
+class Solution:
+    def calculate(self, s: str) -> int:
+        parsed = []
+        curr_num = 0
+        for c in s:
+            if c == " ":
+                continue
+            if c in "+-*/":
+                parsed.append(curr_num)
+                curr_num = 0
+                parsed.append(c)
+            else:
+                curr_num = 10 * curr_num + int(c)
+        parsed.append(curr_num)
+
+        # pass 1 for mult and div
+        stack1 = list(reversed(parsed))
+        stack2 = []
+
+        while stack1:
+            top = stack1.pop()
+            if str(top) in "*/":
+                left = stack2.pop()
+                right = stack1.pop()
+                if top == "*":
+                    stack2.append(left * right)
+                else:
+                    stack2.append(left // right)
+            else:
+                stack2.append(top)
+        
+        # pass 2 for add and sub
+        result = stack2[0]
+        for i in range(1, len(stack2), 2):
+            if stack2[i] == "+":
+                result += stack2[i+1]
+            else:
+                result -= stack2[i+1]
+            
+        return result

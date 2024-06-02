bb18b107c1624eef829d90a5a3c3d9599a74f003
Tanson Lee <leetanson@gmail.com>
2024-06-02 11:04:56 -0700

    938 optimized

--- 938_range_sum_of_bst/main.py
+++ 938_range_sum_of_bst/main.py
@@ -4,20 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-"""
-Possible to further optimize by not looking down subtrees
-that we already know will not be in the range.
-"""
-
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if root is None:
             return 0
-        result = 0
-        if root.val >= low and root.val <= high:
-            result += root.val
-        result += self.rangeSumBST(root.left, low, high)
-        result += self.rangeSumBST(root.right, low, high)
+        if low <= root.val and root.val <= high:
+            return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
 
-        return result 
+

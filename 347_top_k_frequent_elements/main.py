37fe8ac49fa60322950be97af3a070130c8cab99
Tanson Lee <leetanson@gmail.com>
2024-06-02 11:01:22 -0700

    347 buckets

--- 347_top_k_frequent_elements/main.py
+++ 347_top_k_frequent_elements/main.py
@@ -2,6 +2,18 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets = [[] for _ in range(len(nums) + 1)]
+        counts = Counter(nums)
+        for n in counts:
+            buckets[counts[n]].append(n)
+
+        result = []
+        for l in reversed(buckets):
+            if len(result) == k:
+                break
+            result.extend(l)
+        return result
+
         counts = defaultdict(int)
 
         for n in nums:

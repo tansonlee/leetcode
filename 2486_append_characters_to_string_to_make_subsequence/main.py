52840b7d8c00a1f28cf557f0d1bc86bad3acecda
Tanson Lee <leetanson@gmail.com>
2024-06-02 18:42:06 -0700

    2486

---
+++ 2486_append_characters_to_string_to_make_subsequence/main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        ps = 0
+        pt = 0
+
+        while ps < len(s) and pt < len(t):
+            if s[ps] == t[pt]:
+                pt += 1
+            ps += 1
+
+        if pt == len(t):
+            return 0
+        
+        return len(t) - pt
+        
+
+        
+        
+        
+            
+        

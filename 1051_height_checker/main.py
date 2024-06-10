00c47bcea818b6ec9a9149c66375ec6f7a1c1f8b
Tanson Lee <leetanson@gmail.com>
2024-06-09 21:35:30 -0700

    1051

---
+++ 1051_height_checker/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+
+        result = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                result += 1
+        
+        return result
+        

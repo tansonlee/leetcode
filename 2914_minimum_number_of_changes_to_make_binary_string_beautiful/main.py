4660377e2620544e65f344dd43b94dd789e79fe4
Tanson Lee <leetanson@gmail.com>
2024-11-04 22:55:04 -0800

    2914

---
+++ 2914_minimum_number_of_changes_to_make_binary_string_beautiful/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        result = 0
+        
+        for i in range(0, len(s), 2):
+            if s[i] != s[i + 1]:
+                result += 1
+        
+        return result
+        
